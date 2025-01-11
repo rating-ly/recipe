7e8623ed4ecbb8f7f98a5849744a138fbec1f608
297Stay <jenny.huajieyu@gmail.com>
2025-01-11 10:35:14 -0500

    Home Page

--- recipe.py
+++ recipe.py
@@ -1,14 +1,17 @@
 from flask import Flask
-
+from flask import render_template
 #create a Flask application
 #the argument to Flask is the name of the application's module
 #since we are running our application in a single file, leave it as __name__
-app=Flask(__name__)
+app=Flask(__name__, static_folder="static")
 
 #relative path to the root of your application
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('home.html')
 
+@app.route('/printable', methods=['GET'])
+def printable():
+    return "printable"
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
