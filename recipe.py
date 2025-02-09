4d94716639ea606a07a5a18695bed90965590f4f
297Stay <jenny.huajieyu@gmail.com>
2025-02-09 11:08:15 -0500

    Extract title and time

--- recipe.py
+++ recipe.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask import render_template
+from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+import re
 #create a Flask application
 #the argument to Flask is the name of the application's module
 #since we are running our application in a single file, leave it as __name__
@@ -12,6 +15,12 @@ def hello_world():
 
 @app.route('/printable', methods=['GET'])
 def printable():
-    return "printable"
+    return render_template('printable.html')
+
+def scrape():
+    req = Request('https://addapinch.com/the-best-chocolate-cake-recipe-ever/', headers={'User-Agent': 'Mozilla/5.0'})
+    html_page = urlopen(req).read()
+    print(html_page)
+
 if __name__ == '__main__':
     app.run(debug=False)
