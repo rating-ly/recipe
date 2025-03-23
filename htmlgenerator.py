7009c54ae96b5fe68c77b9c40725a052995d4451
Gula Nurmatova <gnurmatova@icloud.com>
2025-03-23 16:57:02 -0400

    changed the printable from pdf to html
    the pdf library used, fpdf was limited. changed to just html for now, then will look for converting from html to pdf

---
+++ htmlgenerator.py
@@ -0,0 +1,26 @@
+import json
+
+def generateHTML(js):
+    
+    html_content = ""
+
+    head = "<head><title>"+js['description']+"</title></head>"
+    html_content +=head
+
+    html_content += "<body>"
+
+    title = "<h1>"+js['description']+"</h1>"
+    html_content += title 
+
+    html_content += "</body>"
+
+    html_content += "<br/><br/><br/><br/><br/><br/>"
+    html_content += json.dumps(js, sort_keys=True, indent=2, separators=(',<br/>', ': '))
+
+    return html_content
+
+
+
+
+
+
--- recipe.py
+++ recipe.py
@@ -1,52 +1,31 @@
 from flask import Flask
 from flask import render_template
-from bs4 import BeautifulSoup
 from flask import request, make_response
-from urllib.request import Request, urlopen
-import re
 import scraper
-import pdf_generator 
+import htmlgenerator
 import json
 
 
-#create a Flask application
-#the argument to Flask is the name of the application's module
-#since we are running our application in a single file, leave it as __name__
 app=Flask(__name__, static_folder="static")
 
-#relative path to the root of your application
 @app.route('/')
 def hello_world():
     return render_template('home.html')
 
 @app.route('/printable', methods=['GET'])
 
-#works for:
-#https://www.allrecipes.com/recipe/158968/spinach-and-feta-turkey-burgers/
-#https://www.allrecipes.com/recipe/10402/the-best-rolled-sugar-cookies/
-
-#breaks for:
-#https://addapinch.com/the-best-chocolate-cake-recipe-ever/
-#https://www.baking-sense.com/2024/03/08/coconut-layer-cake/
-
 def printable():
     json_string = ""
     try:
         urlp = request.args.get('textInput')
-        print(urlp)
         js = scraper.scrape(urlp)
-        print(js)
-        json_string = json.dumps(js)
-        print(json_string)
     except Exception as e:
         print("error: ",e )
 
     response = None
     try:
-        pdf = pdf_generator.json_to_pdf(json_string)
-        response = make_response(pdf.output(dest='S').encode('latin-1'))
-        response.headers['Content-Type'] = 'application/pdf'
-        response.headers['Content-Disposition'] = 'inline; filename=recipe.pdf'
+        html_content = htmlgenerator.generateHTML(js)
+        response = make_response(html_content)
     except Exception as e:
         print("Error rendering PDF: ", e)
         
